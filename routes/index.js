352219c63a55955b946ee82e51880b8ee6eca82a
Diego Montes <diego.montes@gonitro.com>
2020-07-07 19:52:10 +0100

    fix: linting from eslint

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,24 @@
-var express = require('express');
-var router = express.Router();
-var parseCookies = function(request) {
-    var list = {},
-        rc = request.headers.cookie;
+var express = require('express')
+var router = express.Router()
+var parseCookies = function (request) {
+  var list = {}
+  var rc = request.headers.cookie
 
-    rc && rc.split(';').forEach(function( cookie ) {
-        var parts = cookie.split('=');
-        list[parts.shift().trim()] = decodeURI(parts.join('='));
-    });
+  rc && rc.split(';').forEach(function (cookie) {
+    var parts = cookie.split('=')
+    list[parts.shift().trim()] = decodeURI(parts.join('='))
+  })
 
-    return list;
+  return list
 }
 
 /* GET login page. */
-router.get('/', function(req, res, next) {
-  if(parseCookies(req)["jweToken"]){ //if session token is set user has already logged in
-    next();
-  }else{ //session token is not set so serve the login screen
-    res.render('index.html');
+router.get('/', function (req, res, next) {
+  if (parseCookies(req).jweToken) { // if session token is set user has already logged in
+    next()
+  } else { // session token is not set so serve the login screen
+    res.render('index.html')
   }
-});
+})
 
-module.exports = router;
+module.exports = router
