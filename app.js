460911a33d77e5f742924b1eded92d70cbe5654f
David Haven <david.haven@gonitro.com>
2020-07-14 17:15:14 +0200

    fix: post body missing for token refresh

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Config is global to the app
+global.appConfig = require('./config/default');
+
 var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
-var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var cors = require('cors')
 const { createProxyMiddleware } = require('http-proxy-middleware')
@@ -9,29 +11,33 @@ var indexRouter = require('./routes/index')
 var loginRouter = require('./routes/login')
 var healthRouter = require('./routes/health')
 
-
-// Config is global to the app
-global.appConfig = require('./config/default');
-
 var app = express()
+
+const proxy = createProxyMiddleware({
+  target: global.appConfig.upstreamDashboard.url,
+  changeOrigin: true, // for vhosted sites, changes host header to match to target's host
+  logLevel: global.appConfig.logLevel,
+  onProxyReq: function(proxyReq, req, res, options) {
+      if (req.body) {
+          let bodyData = JSON.stringify(req.body);
+          // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+          proxyReq.setHeader('Content-Type','application/json');
+          proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+          // stream the content
+          proxyReq.write(bodyData);
+      }
+  }
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html');
 
-
-const proxy = createProxyMiddleware({
-  target: global.appConfig.upstreamDashboard.url,
-  changeOrigin: true, // for vhosted sites, changes host header to match to target's host
-  logLevel: global.appConfig.logLevel,
-})
-
 app.use(logger('short'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use('/', indexRouter)
 app.use('/login', loginRouter)
 app.use('/health', healthRouter)
