a1a7a6ae6544ae8a35a2afb8bccebe92b348059c
David Haven <david.haven@gonitro.com>
2020-07-16 17:30:56 +0200

    fix: uncomment some changes

--- app.js
+++ app.js
@@ -72,19 +72,19 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', proxy)
 
 // catch 404 and forward to error handler
-//app.use(function (req, res, next) {
-//  next(createError(404))
-//})
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-//app.use(function (err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-//  res.locals.message = err.message
-//  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-//  res.status(err.status || 500)
-//  res.render('error')
-//})
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 module.exports = app
