04462374c24c1eec7ec60f8e8b1d804cb858af72
David Haven <david.haven@gonitro.com>
2020-09-01 15:41:56 +0100

    fix linting and pipelines

--- test/login.js
+++ test/login.js
@@ -1,18 +1,24 @@
-const expect    = require("chai").expect;
-const request = require('request');
+const expect = require('chai').expect
+const request = require('request')
 global.appConfig = require('../config/default')
 
-describe("Proxy login page", function() {
-  it('should return the login UI', function(done) {
-    request('http://localhost:8888' , function(error, response, body) {
-      expect(body).to.include('<p class="text">Kubernetes Dashboard</p>');
-      done();
-    });
-  });
-  it('should respond with code 200', function(done) {
-    request('http://localhost:8888' , function(error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-});
+describe('Proxy login page', function () {
+  it('should return the login UI', function (done) {
+    request('http://localhost:8888', function (error, response, body) {
+      if (error) {
+        done(error)
+      }
+      expect(body).to.include('<p class="text">Kubernetes Dashboard</p>')
+      done()
+    })
+  })
+  it('should respond with code 200', function (done) {
+    request('http://localhost:8888', function (error, response, body) {
+      if (error) {
+        done(error)
+      }
+      expect(response.statusCode).to.equal(200)
+      done()
+    })
+  })
+})
